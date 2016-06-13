fa747a0d7a193971206ffcf4952a7ddfabc70a66
Genaro Madrid <genmadrid@gmail.com>
2016-06-13 00:50:05 -0500

    test and improve Client

--- mifiel/client.py
+++ mifiel/client.py
@@ -4,11 +4,14 @@ class Client:
   def __init__(self, app_id, secret_key):
     self.sandbox = False
     self.auth = ApiAuth(app_id, secret_key)
+    self.base_url = 'https://www.mifiel.com'
 
   def use_sandbox(self):
     self.sandbox = True
+    self.base_url = 'https://sandbox.mifiel.com'
+
+  def set_base_url(self, base_url):
+    self.base_url = base_url
 
   def url(self):
-    if self.sandbox:
-      return 'https://sandbox.mifiel.com/api/v1/{path}'
-    return 'https://www.mifiel.com/api/v1/{path}'
+    return self.base_url + '/api/v1/{path}'
