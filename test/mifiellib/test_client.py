8dbcfd075b04a98bb4a1b0dc41d26e0f1f3c550c
Genaro Madrid <genmadrid@gmail.com>
2018-10-02 14:32:22 -0500

    upgrade packages

--- test/mifiellib/test_client.py
+++ test/mifiellib/test_client.py
@@ -6,12 +6,12 @@ class TestClient(BaseMifielCase):
     self.client = Client('app_id', 'secret')
 
   def test_url(self):
-    self.assertRegexpMatches(self.client.url(), 'www.mifiel')
+    self.assertRegex(self.client.url(), 'www.mifiel')
 
   def test_sandbox(self):
     self.client.use_sandbox()
-    self.assertRegexpMatches(self.client.url(), 'sandbox.mifiel')
+    self.assertRegex(self.client.url(), 'sandbox.mifiel')
 
   def test_base_url(self):
     self.client.set_base_url('http://example.com')
-    self.assertRegexpMatches(self.client.url(), 'example.com')
+    self.assertRegex(self.client.url(), 'example.com')
